58b494d752a0062f83b87beb70f086405ed54c85
hrishi-2407 <hrishibari24@gmail.com>
2025-08-31 14:25:32 +0530

    resolve stash conflict and add multiply function

--- calculator.py
+++ calculator.py
@@ -2,6 +2,11 @@ class Calculator():
     def add(self, a, b):
         return a + b
     
+    def subtact(self, a, b):
+        return a - b
+    
+    def multiply(self, a, b):
+    
 calci = Calculator()
 
 add_result = calci.add(2, 3)
